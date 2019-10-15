2f9d7fe3b2a6bad771edff2e71f7bcde29ffbf30
John Jegard <john.jegard@gmail.com>
2019-10-15 09:17:44 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Brownian-Motion
+
+A gui that has a few control options such as temperature, mass, velocity, on the left side and demonstrates brownian motion on the right side.
---
+++ src/brownian/motion/Application.java
@@ -0,0 +1,17 @@
+
+package brownian.motion;
+
+/**
+ *
+ * @author John Jegard
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
